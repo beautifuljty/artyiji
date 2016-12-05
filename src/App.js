42021e7ac72d7351e9d88c966fa60a31fd4ace76
grguang <945455382@qq.com>
2016-12-05 09:52:06 +0800

    footer

--- src/App.js
+++ src/App.js
@@ -6,26 +6,12 @@ import Footer from './Footer.js'
 class App extends React.Component {
   render () {
     return(
-<<<<<<< HEAD
       <div className='my-warp'>
         <Header />
         <div className='my-main'>
           {this.props.children}
         </div>
         <Footer />
-=======
-      <div>
-<<<<<<< HEAD
-        <Header />
-        {this.props.children}
-        <Footer />
-=======
-        lalalal
-        <h1>66666</h1>
-        <h2>7777</h2>
-        <h3>654654</h3>
->>>>>>> 6afd3ff9f38f2d60e12b71b699ddafa64285de49
->>>>>>> ad29d98e2fcd1bf2bbfc4d1e1ba0af7f714fa289
       </div>
     )
   }
