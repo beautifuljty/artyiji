89a7bdedde4e166fbe54c89e032b11819eface05
grguang <945455382@qq.com>
2016-12-05 09:47:23 +0800

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,11 +4,7 @@ import {Link} from 'react-router'
 class Footer extends React.Component {
   render () {
     return(
-<<<<<<< HEAD
       <div className='my-footer'>
-=======
-      <div>
->>>>>>> ad29d98e2fcd1bf2bbfc4d1e1ba0af7f714fa289
         <Link to='/'>展讯</Link>
         <Link to='tuijian'>推荐</Link>
         <Link to='fabu'>发布</Link>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import {render} from 'react-dom';
 import Routers from './Routers.js'
 
 import First from './First.js'
-<<<<<<< HEAD
-import './header.css'
-=======
 
->>>>>>> ad29d98e2fcd1bf2bbfc4d1e1ba0af7f714fa289
+import './header.css'
+
 render(<First />,document.getElementById('root'));
