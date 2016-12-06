578b0188ca00befe988627685a0134a85cf690cc
sunshineyanghui <1187073986@qq.com>
2016-12-06 14:49:21 +0800

    add

--- src/index.js
+++ src/index.js
@@ -7,12 +7,9 @@ import First from './First.js'
 import './Zhanxun.css'
 import './swiper.min.css';
 import './lunbo.css';
-
 import './header.css'
-<<<<<<< HEAD
 import './Fabu/fabu.css'
-=======
 import './tuijian.css'
->>>>>>> 9d6978fee92bf78e14def59b9eed03f652e0cb13
+
 
 render(<First />,document.getElementById('root'));
